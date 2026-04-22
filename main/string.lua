12acccdfb26cbf3a86eb9f3a1ee0ada040f302dd
huncho <109549513+bleenobtw@users.noreply.github.com>
2026-04-22 22:58:43 +0100

    feat: more string validation

--- main/string.lua
+++ main/string.lua
@@ -3,10 +3,39 @@ local CBaseValidator, newValidator = table.unpack(require "main.base")
 local StringValidator = setmetatable({}, { __index = CBaseValidator })
 StringValidator.__index = StringValidator
 
+--- Ensures that the value is atleast n characters long.
+---@param n number
+---@param message string # The message to return if this validator fails.
 function StringValidator:min(n, message)
   table.insert(self.__refinements, function(value, path)
     if #value < n then
       return false, message or ("[%s] string must be at least %d character(s), got %d"):format(path, n, #value)
+    end
+    return true, value
+  end)
+  return self
+end
+
+--- Ensures that the value is at most n characters long.
+---@param n number
+---@param message string # The message to return if this validator fails.
+function StringValidator:max(n, message)
+  table.insert(self.__refinements, function(value, path)
+    if #value > n then
+      return false, message or ("[%s] string must be at most %d character(s), got %d"):format(path, n, #value)
+    end
+    return true, value
+  end)
+  return self
+end
+
+--- Ensures that the value is exactly n characters long.
+---@param n number
+---@param message string # The message to return if this validator fails.
+function StringValidator:length(n, message)
+  table.insert(self.__refinements, function(value, path)
+    if #value ~= n then
+      return false, message or ("[%s] string must be exactly %d character(s), got %d"):format(path, n, #value)
     end
     return true, value
   end)
