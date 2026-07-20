77a72e808f1b1dc30d40c128b4ac5825e7676974
bleenobtw <109549513+bleenobtw@users.noreply.github.com>
2026-07-20 19:17:14 +0100

    docs: add FiveM installation guide

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,9 @@ version "1.0.0"
 author "Bleeno <https://github.com/bleenobtw/vua>"
 description "A lightweight, chainable, Zod-inspired runtime validation library for FiveM Lua resources."
 
-lua54 "ok"
 fx_version "cerulean"
 game "gta5"
 
-shared_script "@ox_lib/init.lua"
 files {
-  "main/validate.lua",
+  "main/*.lua",
 }
